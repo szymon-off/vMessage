4c5869bc7d8d2be766fb6a5cbac44e10f40715bb
SzymON/OFF <szymon.olszewik@gmail.com>
2025-08-31 14:31:42 +0200

    Enhance MessageCommand with sender/receiver prefixes and suffixes support

--- src/main/java/off/szymon/vMessage/cmd/MessageCommand.java
+++ src/main/java/off/szymon/vMessage/cmd/MessageCommand.java
@@ -10,8 +10,15 @@ import com.velocitypowered.api.proxy.Player;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import off.szymon.vMessage.Config;
 import off.szymon.vMessage.VMessagePlugin;
+import off.szymon.vMessage.compatibility.LuckPermsCompatibilityProvider;
 
 public class MessageCommand {
+
+    private final LuckPermsCompatibilityProvider lp;
+
+    public MessageCommand() {
+        lp = VMessagePlugin.getInstance().getLuckPermsCompatibilityProvider();
+    }
 
     public BrigadierCommand createCommand() {
         return new BrigadierCommand(
@@ -39,16 +46,25 @@ public class MessageCommand {
                                             if (!Config.getYaml().getBoolean("commands.message.allow-minimessage")) {
                                                 message = MiniMessage.miniMessage().escapeTags(message);
                                             }
+
+                                            /* Placholders */
+
+                                            // Player names (sender and receiver), servers (sender and receiver), message
                                             if (senderPlayer != null) {
-                                                String serverName = senderPlayer.getCurrentServer()
+                                                String senderServer = senderPlayer.getCurrentServer()
+                                                        .map(server -> VMessagePlugin.getInstance().getBroadcaster().parseAlias(server.getServerInfo().getName()))
+                                                        .orElse("Unknown");
+                                                String receiverServer = receiver.getCurrentServer()
                                                         .map(server -> VMessagePlugin.getInstance().getBroadcaster().parseAlias(server.getServerInfo().getName()))
                                                         .orElse("Unknown");
                                                 senderFormat = senderFormat
                                                         .replace("%sender%", senderPlayer.getUsername())
-                                                        .replace("%sender-server%", serverName);
+                                                        .replace("%sender-server%", senderServer)
+                                                        .replace("%receiver-server%", receiverServer);
                                                 receiverFormat = receiverFormat
                                                         .replace("%sender%", senderPlayer.getUsername())
-                                                        .replace("%sender-server%", serverName);
+                                                        .replace("%sender-server%", senderServer)
+                                                        .replace("%receiver-server%", receiverServer);
                                             } else {
                                                 senderFormat = senderFormat
                                                         .replace("%sender%", "Console")
@@ -63,6 +79,49 @@ public class MessageCommand {
                                             receiverFormat = receiverFormat
                                                     .replace("%receiver%",receiver.getUsername())
                                                     .replace("%message%",message);
+                                            // Sender and receiver prefixes and suffixes (if LuckPerms is installed)
+                                            if (lp != null) {
+                                                if (senderPlayer != null) {
+                                                    LuckPermsCompatibilityProvider.PlayerData senderData = lp.getMetaData(senderPlayer);
+                                                    senderFormat = senderFormat
+                                                            .replace("%sender-prefix%", senderData.metaData().getPrefix() != null ? senderData.metaData().getPrefix() : "")
+                                                            .replace("%sender-suffix%", senderData.metaData().getSuffix() != null ? senderData.metaData().getSuffix() : "");
+                                                    for (var entry : VMessagePlugin.getInstance().getBroadcaster().getMetaPlaceholders().entrySet()) {
+                                                        senderFormat = senderFormat.replace(
+                                                                entry.getKey(),
+                                                                senderData.metaData().getMetaValue(entry.getValue()) != null ? senderData.metaData().getMetaValue(entry.getValue()) : ""
+                                                        );
+                                                    }
+                                                } else {
+                                                    senderFormat = senderFormat
+                                                            .replace("%sender-prefix%", "")
+                                                            .replace("%sender-suffix%", "");
+                                                }
+                                                LuckPermsCompatibilityProvider.PlayerData receiverData = lp.getMetaData(receiver);
+                                                senderFormat = senderFormat
+                                                        .replace("%receiver-prefix%", receiverData.metaData().getPrefix() != null ? receiverData.metaData().getPrefix() : "")
+                                                        .replace("%receiver-suffix%", receiverData.metaData().getSuffix() != null ? receiverData.metaData().getSuffix() : "");
+                                                receiverFormat = receiverFormat
+                                                        .replace("%receiver-prefix%", receiverData.metaData().getPrefix() != null ? receiverData.metaData().getPrefix() : "")
+                                                        .replace("%receiver-suffix%", receiverData.metaData().getSuffix() != null ? receiverData.metaData().getSuffix() : "");
+                                                for (var entry : VMessagePlugin.getInstance().getBroadcaster().getMetaPlaceholders().entrySet()) {
+                                                    senderFormat = senderFormat.replace(
+                                                            entry.getKey(),
+                                                            receiverData.metaData().getMetaValue(entry.getValue()) != null ? receiverData.metaData().getMetaValue(entry.getValue()) : ""
+                                                    );
+                                                    receiverFormat = receiverFormat.replace(
+                                                            entry.getKey(),
+                                                            receiverData.metaData().getMetaValue(entry.getValue()) != null ? receiverData.metaData().getMetaValue(entry.getValue()) : ""
+                                                    );
+                                                }
+                                            } else {
+                                                senderFormat = senderFormat
+                                                        .replace("%sender-prefix%", "")
+                                                        .replace("%sender-suffix%", "")
+                                                        .replace("%receiver-prefix%", "")
+                                                        .replace("%receiver-suffix%", "");
+                                            }
+
                                             sender.sendRichMessage(senderFormat);
                                             receiver.sendRichMessage(receiverFormat);
                                             return Command.SINGLE_SUCCESS;
