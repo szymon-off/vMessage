67be4768f682b41e2be78083eecd52332f6c4884
SzymON/OFF <szymon.olszewik@gmail.com>
2026-09-21 22:04:54 +0200

    Refactor VMessageCommand to improve argument handling and case sensitivity in player lookups

--- src/main/kotlin/off/szymon/vmessage/command/VMessageCommand.kt
+++ src/main/kotlin/off/szymon/vmessage/command/VMessageCommand.kt
@@ -18,7 +18,6 @@ import com.velocitypowered.api.command.BrigadierCommand
 import off.szymon.fishy.api.FishyAPI
 import off.szymon.vmessage.VMessage
 import off.szymon.vmessage.message.HandlerManager
-import off.szymon.vmessage.message.handler.ChatHandler
 
 class VMessageCommand : PluginCommand("vmessage", "vmsg", "vm") {
 
@@ -51,8 +50,8 @@ class VMessageCommand : PluginCommand("vmessage", "vmsg", "vm") {
                         .then(BrigadierCommand.requiredArgumentBuilder("player", StringArgumentType.word())
                             .executes { ctx ->
                                 @Suppress("DuplicatedCode")
-                                val playerString = ctx.getArgument("player", String::class.java).lowercase()
-                                val player = VMessage.get().proxy.allPlayers.find { it.username.lowercase() == playerString } ?: run {
+                                val playerString = ctx.getArgument("player", String::class.java)
+                                val player = VMessage.get().proxy.allPlayers.find { it.username.equals(playerString, ignoreCase = true) } ?: run {
                                     sendMessage(ctx.source, "<dark_gray>▎</dark_gray><gray>Invalid argument provided for '<#00ffff>player</#00ffff>'</gray>")
                                     return@executes Command.SINGLE_SUCCESS // handled properly
                                 }
@@ -63,33 +62,39 @@ class VMessageCommand : PluginCommand("vmessage", "vmsg", "vm") {
                                     return@executes Command.SINGLE_SUCCESS // handled properly
                                 }
 
+                                if (handler.requiresMessage) {
+                                    sendMessage(ctx.source, "<dark_gray>▎</dark_gray><gray>Not enough arguments provided for '<#00ffff>$type</#00ffff>'</gray>")
+                                    return@executes Command.SINGLE_SUCCESS // handled properly
+                                }
+
                                 handler.broadcast(player)
                                 sendMessage(ctx.source, "<dark_gray>▎</dark_gray><gray>Sent fake '<#00ffff>$type</#00ffff>' message as</gray> <#00ffff>${player.username}</#00ffff>")
                                 return@executes Command.SINGLE_SUCCESS
                             }
                             .then(BrigadierCommand.requiredArgumentBuilder("message", StringArgumentType.greedyString())
                                 .executes { ctx ->
-                                    val type = ctx.getArgument("type", String::class.java).lowercase()
-                                    if (type != "chat") {
-                                        sendMessage(ctx.source, "<dark_gray>▎</dark_gray><gray>Too many arguments provided for '<#00ffff>$type</#00ffff>'</gray>")
-                                        return@executes Command.SINGLE_SUCCESS // handled properly
-                                    }
-
-                                    val playerString = ctx.getArgument("player", String::class.java).lowercase()
-                                    val player = VMessage.get().proxy.allPlayers.find { it.username == playerString } ?: run {
+                                    @Suppress("DuplicatedCode")
+                                    val playerString = ctx.getArgument("player", String::class.java)
+                                    val player = VMessage.get().proxy.allPlayers.find { it.username.equals(playerString, ignoreCase = true) } ?: run {
                                         sendMessage(ctx.source, "<dark_gray>▎</dark_gray><gray>Invalid argument provided for '<#00ffff>player</#00ffff>'</gray>")
                                         return@executes Command.SINGLE_SUCCESS // handled properly
                                     }
 
-                                    val message = ctx.getArgument("message", String::class.java).lowercase()
-
-                                    val handler = HandlerManager.get().getHandler(ChatHandler::class.java) ?: run {
+                                    val type = ctx.getArgument("type", String::class.java).lowercase()
+                                    val handler = HandlerManager.get().getHandler(type) ?: run {
                                         sendMessage(ctx.source, "<dark_gray>▎</dark_gray><gray>Invalid argument provided for '<#00ffff>type</#00ffff>'</gray>")
                                         return@executes Command.SINGLE_SUCCESS // handled properly
                                     }
 
+                                    if (!handler.requiresMessage) {
+                                        sendMessage(ctx.source, "<dark_gray>▎</dark_gray><gray>Too many arguments provided for '<#00ffff>$type</#00ffff>'</gray>")
+                                        return@executes Command.SINGLE_SUCCESS // handled properly
+                                    }
+
+                                    val message = ctx.getArgument("message", String::class.java)
+
                                     handler.broadcast(player, message)
-                                    sendMessage(ctx.source, "<dark_gray>▎</dark_gray><gray>Sent fake '<#00ffff>chat</#00ffff>' message as</gray> <#00ffff>${player.username}</#00ffff>")
+                                    sendMessage(ctx.source, "<dark_gray>▎</dark_gray><gray>Sent fake '<#00ffff>$type</#00ffff>' message as</gray> <#00ffff>${player.username}</#00ffff>")
 
                                     return@executes Command.SINGLE_SUCCESS
                                 }
